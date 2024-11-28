29aa99971c09d276569b950ec57016fe28e5e963
Jiaye Lee <charlotte@Mac.lan>
2024-11-27 19:26:49 -0500

    initial_version

---
+++ src/application/MainController.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class MainController {
+
+	
+	  public void switchScene(ActionEvent event, String fxmlFile) {
+	        try {
+	            Parent newPage = FXMLLoader.load(getClass().getResource(fxmlFile));
+	            Scene newScene = new Scene(newPage);
+	            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	            stage.setScene(newScene);
+	            stage.show();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    public void goToSecondPage(ActionEvent event) {
+	        switchScene(event, "CreateRecipe.fxml");
+	    }
+	    
+	    public void goBackPage(ActionEvent event) {
+	        switchScene(event, "Admin_home.fxml");
+	    }
+
+}
