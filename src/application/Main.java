1a18713c5a5794925c8a8ede52c1a8fd73c17ccb
Jiaye Lee <charlotte@Mac.lan>
2024-11-28 13:38:56 -0500

    UI and PageConnection completed

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("User_home.fxml"));
-			Scene scene = new Scene(root,600,410);
+			Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+			Scene scene = new Scene(root,600,400);
 			
 			primaryStage.setScene(scene);
 			primaryStage.show();
